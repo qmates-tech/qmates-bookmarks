c5c6b1d1eb024a56f9ddccd67591ed5c28505c74
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2025-05-30 16:24:44 +0200

    extract SVGs as files

--- src/pages/homepage.ts
+++ src/pages/homepage.ts
@@ -17,20 +17,7 @@ const bookmarkListItem: HtmlElement<Bookmark> = (bookmark) => {
       <div class="absolute top-2 right-2">
         <div class="dropdown dropdown-end">
           <div tabindex="0" role="button" class="btn btn-ghost btn-sm btn-circle z-10">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
-              />
-            </svg>
+            <image src="/static/img/vertical-dots.svg" alt="more options" class="w-6 h-6" />
           </div>
           <ul tabindex="0" class="dropdown-content z-[50] menu p-2 shadow bg-base-100 rounded-box w-52">
             <li><a>Share</a></li>
--- src/pages/layout.ts
+++ src/pages/layout.ts
@@ -22,21 +22,11 @@ export const layout: HtmlElement<{ title: string; content: string }> = ({ title,
             <div class="flex gap-1 sm:gap-2">
               <div class="tooltip tooltip-bottom" data-tip="save new bookmark">
                 <button class="btn btn-ghost btn-circle btn-sm sm:btn-md">
-                  <svg
+                  <image
+                    src="/static/img/bookmark.svg"
+                    alt="add bookmark"
                     class="w-5 h-5 text-gray-800 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 20"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="m13 19-6-5-6 5V2a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v17Z"
-                    ></path>
-                  </svg>
+                  />
                 </button>
               </div>
               !${search()}
@@ -52,12 +42,7 @@ export const layout: HtmlElement<{ title: string; content: string }> = ({ title,
 function search() {
   return html`
     <form class="input input-sm sm:input-md max-w-[200px] sm:max-w-xs">
-      <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor">
-          <circle cx="11" cy="11" r="8"></circle>
-          <path d="m21 21-4.3-4.3"></path>
-        </g>
-      </svg>
+      <image class="h-[1em] opacity-50" src="/static/img/search.svg" alt="search" />
       <input
         type="search"
         name="search"
