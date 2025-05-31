7246b78672e58c98b1bc798d31eb9737e3fe1ca7
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2025-05-31 15:06:43 +0200

    simplify a lot the html classes

--- src/pages/homepage.ts
+++ src/pages/homepage.ts
@@ -5,7 +5,7 @@ import { generatePlaceholderImage } from '../image-generator'
 
 export const homepage: HtmlElement<Bookmark[]> = (bookmarks) => {
   return html`
-    <ul class="bookmarks grid grid-cols-1 md:grid-cols-3 gap-0 md:gap-4">
+    <ul class="bookmark-list">
       !${bookmarks.map(bookmarkListItem)}
     </ul>
   `
@@ -13,37 +13,26 @@ export const homepage: HtmlElement<Bookmark[]> = (bookmarks) => {
 
 const bookmarkListItem: HtmlElement<Bookmark> = (bookmark) => {
   return html`
-    <li class="h-full relative bg-base-100 md:shadow-sm md:rounded-lg">
+    <li class="relative">
       <div class="absolute top-2 right-2">
         <div class="dropdown dropdown-end">
-          <div tabindex="0" role="button" class="btn btn-ghost btn-sm btn-circle z-10">
+          <button tabindex="0">
             <image src="/static/img/vertical-dots.svg" alt="more options" class="w-6 h-6" />
-          </div>
-          <ul tabindex="0" class="dropdown-content z-[50] menu p-2 shadow bg-base-100 rounded-box w-52">
+          </button>
+          <ul tabindex="0" class="dropdown-content">
             <li><a>Share</a></li>
-            <li class="!border-none after:!hidden"><a class="text-error">Delete</a></li>
+            <li><a class="text-error">Delete</a></li>
           </ul>
         </div>
       </div>
 
-      <a
-        href="${bookmark.url}"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="flex flex-row md:flex md:flex-col h-full"
-      >
-        <figure class="w-[80px] h-[80px] md:w-[200px] md:h-[150px] md:w-full p-3 md:p-0 flex-shrink-0">
-          <img
-            src="${generatePlaceholderImage(bookmark.title)}"
-            alt="${bookmark.title}"
-            class="object-cover w-full h-full rounded-lg md:rounded-t-md md:rounded-b-none"
-          />
+      <a href="${bookmark.url}" target="_blank" rel="noopener noreferrer" class="bookmark">
+        <figure>
+          <img src="${generatePlaceholderImage(bookmark.title)}" alt="${bookmark.title}" />
         </figure>
-        <div class="flex-1 py-3 pl-0 pr-3 md:p-4 flex flex-col">
-          <h2 class="max-md:pr-5 md:font-bold">${bookmark.title}</h2>
-          <div class="md:text-right md:mt-auto">
-            <span class="opacity-45">site-name.it</span>
-          </div>
+        <div class="bookmark-content">
+          <h2>${bookmark.title}</h2>
+          <div>site-name.it</div>
         </div>
       </a>
     </li>
--- src/pages/layout.ts
+++ src/pages/layout.ts
@@ -9,23 +9,18 @@ export const layout: HtmlElement<{ title: string; content: string }> = ({ title,
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <title>${title}</title>
-        <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         <link href="/static/css/style.css" rel="stylesheet" type="text/css" />
       </head>
       <body>
-        <div class="shadow-sm z-20 fixed w-full">
-          <div class="navbar bg-base-100 max-w-5xl mx-auto px-4 sm:px-6 md:px-8">
-            <div class="flex-1">
-              <a href="/" class="text-lg sm:text-xl font-medium">Bookmarks</a>
+        <div class="navigation">
+          <div class="navbar container">
+            <div class="logo">
+              <a href="/" class="logo">Bookmarks</a>
             </div>
-            <div class="flex gap-1 sm:gap-2">
-              <div class="tooltip tooltip-bottom" data-tip="save new bookmark">!${addBookmark()}</div>
-              !${search()}
-            </div>
+            <div class="search">!${addBookmark()} !${search()}</div>
           </div>
         </div>
-        <div class="p-4 md:p-8 pt-20 md:pt-24 max-w-5xl mx-auto">!${content}</div>
+        <div class="container page">!${content}</div>
       </body>
     </html>
   `
@@ -33,59 +28,60 @@ export const layout: HtmlElement<{ title: string; content: string }> = ({ title,
 
 function addBookmark() {
   return html`
-    <button onclick="showAddBookmarkModal()" class="btn btn-ghost btn-circle btn-sm sm:btn-md">
-      <image src="/static/img/bookmark.svg" alt="add bookmark" class="w-5 h-5 text-gray-800 dark:text-white" />
-    </button>
-    <script>
-      function showAddBookmarkModal() {
-        add_bookmark_modal.showModal()
-        setTimeout(() => document.getElementById('add-bookmark-url').focus(), 100)
-      }
-    </script>
-    <dialog id="add_bookmark_modal" class="modal sm:modal-middle">
-      <div class="modal-box">
-        <form method="dialog">
-          <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-        </form>
-        <h3 class="text-lg font-bold">Save a Bookmark!</h3>
-        <div class="join pt-7 w-full mx-auto">
-          <div class="w-full">
-            <label class="input validator w-full">
-              <image class="h-[1em] opacity-50" src="/static/img/link.svg" alt="bookmark" />
-              <input
-                id="add-bookmark-url"
-                type="url"
-                required
-                placeholder="https://"
-                pattern="^(https?://)?([a-zA-Z0-9]([a-zA-Z0-9\\-].*[a-zA-Z0-9])?\\.)+[a-zA-Z].*$"
-                title="Must be valid URL"
-              />
-            </label>
-            <p class="validator-hint">Must be valid URL</p>
+    <div class="add-bookmark tooltip tooltip-bottom" data-tip="save new bookmark">
+      <button onclick="showAddBookmarkModal()">
+        <img src="/static/img/bookmark.svg" alt="add bookmark" />
+      </button>
+      <script>
+        function showAddBookmarkModal() {
+          add_bookmark_modal.showModal()
+          setTimeout(() => document.getElementById('add-bookmark-url').focus(), 100)
+        }
+      </script>
+      <dialog id="add_bookmark_modal" class="modal sm:modal-middle">
+        <div class="modal-box">
+          <form method="dialog">
+            <button class="close">✕</button>
+          </form>
+          <h3>Save a Bookmark!</h3>
+          <div class="join-input-with-validator">
+            <div>
+              <label>
+                <img src="/static/img/link.svg" alt="bookmark" />
+                <input
+                  id="add-bookmark-url"
+                  type="url"
+                  required
+                  placeholder="https://"
+                  pattern="^(https?://)?([a-zA-Z0-9]([a-zA-Z0-9\\-].*[a-zA-Z0-9])?\\.)+[a-zA-Z].*$"
+                  title="Must be valid URL"
+                />
+              </label>
+              <p class="validator-hint">Must be valid URL</p>
+            </div>
+            <button class="input-button">Save</button>
           </div>
-          <button class="btn btn-neutral join-item">Save</button>
         </div>
-      </div>
-      <form method="dialog" class="modal-backdrop">
-        <button>close</button>
-      </form>
-    </dialog>
+        <form method="dialog" class="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </div>
   `
 }
 
 function search() {
   return html`
-    <form class="input input-sm sm:input-md max-w-[200px] sm:max-w-xs">
-      <image class="h-[1em] opacity-50" src="/static/img/search.svg" alt="search" />
+    <form class="search-form">
+      <img src="/static/img/search.svg" alt="search" />
       <input
         type="search"
         name="search"
         onload="togglePressEnterBadge(this)"
         oninput="togglePressEnterBadge(this)"
-        class="grow text-sm sm:text-base"
         placeholder="Search"
       />
-      <kbd class="kbd kbd-xs sm:kbd-sm md:kbd-md invisible">↵</kbd>
+      <kbd class="invisible">↵</kbd>
     </form>
     <script>
       function togglePressEnterBadge(target) {
